2995746e2afd357e193b6c3d8f8a45b67f9bb642
StereoPT <stereopt@gmail.com>
2015-11-18 22:33:13 +0000

    Register & Intent

--- app/src/main/java/pt/zwame/trivia/LoginActivity.java
+++ app/src/main/java/pt/zwame/trivia/LoginActivity.java
@@ -1,7 +1,9 @@
 package pt.zwame.trivia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void btn_register(View view) {
+        Intent rActivity = new Intent(this, RegisterActivity.class);
+        startActivity(rActivity);
     }
 }
